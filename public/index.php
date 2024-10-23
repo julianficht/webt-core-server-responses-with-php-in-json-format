e38050b66e05def9c7ff0019ae36a6dda6e87d62
julianficht <1126@htl.rennweg.at>
2024-10-23 16:14:00 +0200

    update data

--- public/index.php
+++ public/index.php
@@ -2,13 +2,15 @@
 require_once '../src/OST.php';
 require_once '../src/Song.php';
 
-$ost = new OST(1, "Legend of Zelda OST", "Legend of Zelda", 1986);
+$ost = new OST(2, "Super Mario OST", "Super Mario Bros", 1985);
 
-$song1 = new Song(1, "Main Theme", "Koji Kondo", 1, "2:30");
-$song2 = new Song(2, "Dungeon Theme", "Koji Kondo", 2, "1:45");
+$song1 = new Song(1, "Overworld Theme", "Koji Kondo", 1, "3:15");
+$song2 = new Song(2, "Underground Theme", "Koji Kondo", 2, "2:00");
+$song3 = new Song(3, "Castle Theme", "Koji Kondo", 3, "1:45");
 
 $ost->addSong($song1);
 $ost->addSong($song2);
+$ost->addSong($song3);
 
 header('Content-type: application/json');
 echo json_encode($ost->getOSTData(), JSON_PRETTY_PRINT);
