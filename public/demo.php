7624dc6fe96030240efd0d9ee5391a5faf9f40b8
julianficht <1126@htl.rennweg.at>
2024-10-23 20:43:25 +0200

    final

--- public/demo.php
+++ public/demo.php
@@ -1,11 +1,22 @@
 <?php
-
 require_once '../src/Seeder.php';
 
 $osts = Seeder::seed();
 
-header('Content-type: application/json');
+if (isset($_GET['id'])) {
+    $id = (int) $_GET['id'];
 
-echo json_encode(array_map(function ($ost) {
-    return $ost->getOSTData();
-}, $osts), JSON_PRETTY_PRINT);
+    foreach ($osts as $ost) {
+        if ($ost->getOSTData()['id'] === $id) {
+            header('Content-type: application/json');
+            echo json_encode($ost->getOSTData(), JSON_PRETTY_PRINT);
+            exit;
+        }
+    }
+    echo "Error: The OST with the id {$id} doesn't exist!";
+} else {
+    header('Content-type: application/json');
+    echo json_encode(array_map(function($ost) {
+        return $ost->getOSTData();
+    }, $osts), JSON_PRETTY_PRINT);
+}
